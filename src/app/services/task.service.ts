d16f6348cd3e3d25ad39e0895a091bb363f9f27a
Jonas Campos <jonascampos06@gmail.com>
2024-12-10 19:40:49 -0300

    Correção import Tarefa no task service

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Tarefa } from '../../tarefa';
+import { Tarefa } from '../../Tarefa';
 
 @Injectable({
   providedIn: 'root'
